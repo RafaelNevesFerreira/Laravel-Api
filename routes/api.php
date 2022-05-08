9dca0e65a519bfc4ffa7fea01a95c8a831a5846b
rafael ferreira <contact@ferreirarafael.com>
2022-05-08 14:34:36 +0200

    suppresion des dossiers css et js

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get("/", function () {
+    return response()->json(
+        [
+            "status" => 200,
+            "success" => true,
+            "message" => "Bienvenue",
+            "data" => [
+                "service" => "News Api",
+                "version" => "1.0",
+                "langue" => app()->getLocale(),
+                "support" => "contact@ferreirarafael.com"
+            ]
+        ]
+    );
 });
